7abcfbeaf732c7bc7c7fdc54b101da041089d450
Cheyenne Piper <robeltopiper@gmail.com>
2023-03-22 02:46:27 -0400

    started intial design

---
+++ app.js
@@ -0,0 +1,14 @@
+const apiKey='at_y1UaYqGp4IpQIqOmiB8DHq0jkeQjC';
+const ipAddress='192.206.151.131';
+const test =document.querySelector('h2');;
+
+
+// fetch(`https://geo.ipify.org/api/v2/country?apiKey=${apiKey}&ipAddress=${ipAddress}`)
+//   .then(response => response.json())
+//   .then(data => {
+//     console.log(data);
+//     // do something with the data, like update the UI
+//     test.innerHTML=data.isp
+//   })
+//   .catch(error => console.error(error));
+
